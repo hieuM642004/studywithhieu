ae0eb1fbe0177b1b801a4505eafeb84a76669577
duynhpc05141 <duynhpc05141@fpt.edu.vn>
2024-05-14 01:04:04 +0700

    [FE]_Created Populer pages

---
+++ frontend/src/app/pages/populer/populer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-populer',
+  standalone: true,
+  imports: [],
+  templateUrl: './populer.component.html',
+  styleUrl: './populer.component.scss'
+})
+export class PopulerComponent {
+
+}
